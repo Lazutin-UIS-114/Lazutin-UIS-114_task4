c4f59f6e1ab68d18f331178a8a6721a61a922fb3
Lazutin-UIS-114 <80814794+Lazutin-UIS-114@users.noreply.github.com>
2021-10-13 21:18:41 +0300

    Update function.h

--- function.h
+++ function.h
@@ -1,43 +1,89 @@
 ﻿#pragma once
 #include <iostream>
+
 using namespace std;
 
-class function { //базовый класс
+/**
+* \brief Класс function для представления объекта многочлен 3 степени
+*/
+class function { 
 private:
 	float a;
 	float b;
 	float c;
 public:
-	void set_a(float a);
-
-	void set_b(float b);
-
-	void set_c(float c);
-
+	/**
+	* \brief установление значения коэффициента a
+        */
+	void set_a(const float a);
+	
+	/**
+	* \brief установление значения коэффициента b
+        */
+	void set_b(const float b);
+	
+	/**
+	* \brief установление значения коэффициента c
+        */
+	void set_c(const float c);
+	
+	/**
+	* \brief получение коэффициента a
+        */
 	float get_a() const;
-
+	
+	/**
+	* \brief получение коэффициента b
+        */
 	float get_b() const;
-
+	
+	/**
+	* \brief получение коэффициента c
+        */
 	float get_c() const;
 
-	function(float a = 1, float b = 1, float c = 1);
+	/**
+	* \brief конструктор функции-многочлена
+        */
+	function(const float a = 1, const float b = 1, const float c = 1);
 	
+	/**
+	* \brief получение производной функции
+        */
 	function derivative();
 };
 
+/**
+* \brief перегруженный оператор вывода для функции
+*/
 ostream& operator<<(ostream& out, const function& function);
 
+/**
+* \brief Класс function для представления объекта "парабола"
+*/
 class parabola : public function { //класс парабола
 public:
-	parabola(float a = 1, float b = 0, float c = 0);
+	parabola(const float a = 1, const float b = 0, const float c = 0);
 };
 
-class linear_function : public function { //класс линейная функция
+/**
+* \brief Класс function для представления объекта "линейная функция"
+*/
+class linear_function : public function { 
 public:
-	linear_function(float a = 1, float b = 0); 
+	/**
+	* \brief конструктор линейной функции
+        */
+	linear_function(const float a = 1, const float b = 0); 
 };
 
-class constant : public function { //класс константа
+/**
+* \brief Класс function для представления объекта "константа"
+*/
+class constant : public function { 
 public:
-	constant(float a = 1);
+	/**
+	* \brief конструктор константы
+        */
+	constant(const float a = 1);
 };
